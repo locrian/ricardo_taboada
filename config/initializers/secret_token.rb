d3c3f3aa76ff5438b08d4b542aa7419cc7d6c7f0
ricardo.taboada@hotmail.com <rft@mint>
2012-09-25 15:54:43 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RicardoTaboada::Application.config.secret_token = 'a7f3ab789c337d42a2806b5945c0c34aef49bfb0e3b8bc04246027f82486a314a12f6207a4c2502dab8725e189d0174adb1515462e3d6aa54e151a318e139656'
