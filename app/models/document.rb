dfdb2ca71fba62ccd484510626581300a4565a61
ricardo.taboada@hotmail.com <rft@mint>
2013-03-12 00:23:00 +0000

    same as last commit

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,7 +4,7 @@ class Document < ActiveRecord::Base
   has_attached_file :file,
       :storage => :s3,
       :s3_credentials => {
-        :bucket => ENV['AWS_BUCKET'],
+        :bucket => ENV['S3_BUCKET_NAME'],
         :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
         :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
       }
